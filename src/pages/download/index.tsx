0654f6fe00242170deb680086f3a8504731dd522
LuckyZacha <zachatatem@gmail.com>
2022-11-17 05:13:22 -0500

    Final version

--- src/pages/download/index.tsx
+++ src/pages/download/index.tsx
@@ -1,14 +1,56 @@
 import Link from 'next/link'
 import { useSelector, useDispatch } from 'react-redux'
 import { saveAs } from 'file-saver';
-import JSZip from 'jszip';
+import JSZip, { forEach } from 'jszip';
 import { getRules } from '../../state/rule/reducer'
 import { getPersons } from '../../state/person/reducer';
 import { RootState, AppDispatch } from '../../state/store'
 
-const getRandom = (arr: Array<String>) => {
-    return arr[Math.floor(Math.random() * arr.length)];
-} 
+const received = new Map<string, Boolean>();
+const given = new Map<string, Boolean>();
+const relation = new Map<string, string>();
+let answer = new Map<string, string>();
+
+
+const init = (persons: Array<{ id: string, name: string }>) => {
+    for (const { name } of persons) {
+        received.set(name, false);
+        given.set(name, false);
+        relation.set(name, '');
+    }
+}
+
+const dfs = (persons: Array<{ id: string, name: string }>, maptogive: Map<string, Array<string>>) => {
+    let allReceived = true;
+    for (const { name } of persons) {
+        if (received.get(name) == false) {
+            allReceived = false;
+            break;
+        }
+    }
+    if (allReceived) {
+        answer = JSON.parse(JSON.stringify(Array.from(relation)));
+        return;
+    }
+
+    for (const { name: nameToGive } of persons) {
+        if (given.get(nameToGive)) continue;
+
+        for (const { name: nameToReceive } of persons) {
+            if (received.get(nameToReceive)) continue;
+            if (maptogive.get(nameToGive)?.includes(nameToReceive) && relation.get(nameToReceive) !== nameToGive) {
+                received.set(nameToReceive, true);
+                given.set(nameToGive, true);
+                relation.set(nameToGive, nameToReceive);
+                dfs(persons, maptogive);
+                received.set(nameToReceive, false);
+                given.set(nameToGive, false);
+                relation.set(nameToGive, '');
+            }
+        }
+
+    }
+}
 
 const DownloadPage = () => {
     const rules = useSelector(({ rule }: RootState) => getRules(rule.rules))
@@ -28,20 +70,29 @@ const DownloadPage = () => {
                 map.set(secondPerson, arr)
             }
         })
-
+        let maptogive = new Map<string, Array<string>>();
         for (const { name } of persons) {
             const relation = map.get(name) as Array<string> ?? []
             relation.push(name)
             const canGive = persons.filter((person) => {
                 return !relation.includes(person.name)
             })
-            let result = name + ", you are giving a present to "
-            const names = canGive.map(({ name }) => name)
-            if(names.length > 0) result += names[0]
-            result += '.'
-            const blob = new Blob([result], { type: "text/plain;charset=utf-8" });
-            zip.file(`${name}.txt`, blob);
+            let arr = maptogive.get(name) as Array<string>
+            if (!arr) arr = []
+            canGive.map(e => arr.push(e.name))
+            maptogive.set(name, arr);
         }
+
+        init(persons);
+        dfs(persons, maptogive);
+        // console.log('relation', answer)
+        answer.forEach(e => {
+            const giver = e[0]
+            const receiver = e[1]
+            let result = giver + ", you are giving a present to " + receiver
+            const blob = new Blob([result], { type: "text/plain;charset=utf-8" });
+            zip.file(`${giver}.txt`, blob);
+        })
         zip.generateAsync({ type: "blob" }).then(function (content) {
             // see FileSaver.js
             saveAs(content, "secretsanta.zip");
